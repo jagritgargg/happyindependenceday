ccc83a3fb0108cf38a3fed958341d524a2b3b628
jagritgargg <100864296+jagritgargg@users.noreply.github.com>
2022-08-14 20:17:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const independence = document.getElementById("independence");
+
+independence.onclick=()=>{
+    //action 1
+    location.href="./celebration.html"
+    //action 2
+    Audio.play();
+}
